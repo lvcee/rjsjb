578486f7c279247bd77ef7367f4daddf7d867af6
Lvce <lveagle@qq.com>
2024-11-06 00:25:08 +0800

    none

--- webpack---adblockplus/src/unload-cleanup/shared/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./unload-cleanup";
-export * from "./unload-cleanup.types";
--- webpack---adblockplus/src/unload-cleanup/shared/unload-cleanup.types.ts
+++
@@ -1,5 +0,0 @@
-export var DisplayValue;
-(function (DisplayValue) {
-    DisplayValue["block"] = "block";
-})(DisplayValue || (DisplayValue = {}));
-export const displayValueList = Object.values(DisplayValue);
